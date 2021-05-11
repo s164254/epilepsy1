3b1c4decf55843e59b5c0b22444c61a690e276f4
Philip <s164254@student.dtu.dk>
2021-05-11 21:42:20 +0200

    cleanup, updated comments

--- patient_plot_full_csi.m
+++ patient_plot_full_csi.m
@@ -4,14 +4,8 @@ pid = '10';
 patient = load_patient(pid,params);
 samples = patient.samples;
 
-
-%samples = patient.samples(1:3*15*60*params.sf);
-%rr = calc_rr_no_fill(qrs_detect_func,samples,params);
-%rr=calc_rr_pan_tompkin_no_rr_filling(samples,params);
-
 temp=qrs_detect3_(samples,params,make_butter(2,[5/(params.sf/2) 30/(params.sf/2)]));
 
-%temp=pan_tompkin_(samples,params);
 rr=calc_HRV(temp,params); %rr intervals in s.
 
 time=cumsum(rr);
@@ -29,11 +23,7 @@ xline((patient.episodes(1,1)+patient.episodes(1,2))/params.sf,'r')
 hold off
 
 
-
-%plot(rr_time_axis,rr);
-
-
-csi = calc_csi_for_sending(patient,params,rr,100);
+csi = calc_csi_send(patient,params,rr,100);
 
 
 %Maximal values in either the first half of the data, or the first 24 hours
@@ -61,73 +51,11 @@ xlabel('Time [s]')
 ylabel('modCSI100filtered*slope')
 hold off
 
-
-hold on
-subplot(2,1,1);
-plot(csi.CSI);
-title('CSI100*slope for patient 5')
-xlabel('Time [s]') 
-ylabel('CSI100*slope')
-subplot(2,1,2);
-plot(csi.modCSI);
-title('modCSI100filtered*slope for patient 5')
-xlabel('Time [s]') 
-ylabel('modCSI100filtered*slope')
-hold off
-
 %Saving rr data for JAVA.
 
 save('RRdata_pat16.mat','temp','rr','rr_time_axis');
 
 
-% Highest modCSI peak located at between index 75000 and 77000 in the
-% signal. This is 75000/202863=36.97% into the signal. We investigate the
-% RR interval detection 36.97% into the raw signal.
-
-%csi_snip = calc_csi(patient,params,rr(74000:77000),100);
-
-plot(csi_snip.modCSI)
-
-
-%x-value 32078
-
-
-%Using the Pan-Tompkin algorithm on the ecg data where manuel annotations
-%were made, in order investigate if there is a problem with the algorithm.
-
-i1=patient.episodes(1,1)+1;
-i2=patient.episodes(1,1)+patient.episodes(1,2);
-rr_seizure_snip=calc_rr_pan_tompkin_no_rr_filling(samples(i1:i2), params);
-rr_normal_snip=calc_rr_pan_tompkin_no_rr_filling(samples(1000000:1000000+patient.episodes(1,2)), params);
-seizure_CSI_snip=calc_csi(patient,params,rr_seizure_snip, 100);
-normal_CSI_snip=calc_csi(patient,params,rr_normal_snip, 100);
-
-hold on
-plot(seizure_CSI_snip.modCSI,'DisplayName','seizure')
-plot(normal_CSI_snip.modCSI,'DisplayName','normal')
-hold off
-legend
-
-%Almost identical to the manually obtained results, so it seems that our
-%Pan Tompkin algorithm works just fine.
-length(samples(i1:i2))
-
-
-%% Testing the original Pan tompkins algorithm from the first hand in
-load s_b_coeff.mat;
-
-RR_new=s_detect_qrs(samples,b_low,b_high,b_avg,delay);
-
-HRV=get_HRV(RR_new,1000,512);
-
-plot(HRV)
-
-%It does not seem to work very well (too many spikes). The one from mathworks seems to be the
-%best one.
-
-
-%Test the ecg_noisecancellation function when you have obtained the p-wave
-%function.
 
 
 
--- run_compare_detect.m
+++ run_compare_detect.m
@@ -12,8 +12,8 @@ samples = patient.samples(start_idx:start_idx+win_size-1);
 f1 = make_sombrero(params.sf);
 f2 = make_butter(2,[5/(params.sf/2) 30/(params.sf/2)]); %n (the first input in butter (make_butter in this case)
 %represents one-half the filter order for a band-pass butterworth filter.
-%Thus, by using order 4, it is actually 8. However, using filtfilt might double the order again,
-%so that the true order is 16. Ask if this is the case. The cut-off frequencies
+%Thus, by using order 2, it is actually 4. However, using filtfilt might double the order again,
+%so that the true order is 8. Ask if this is the case. The cut-off frequencies
 %represent fractions of the Nyquist frequency, which is half the sampling
 %frequency. Thus, we are filtering between 5 Hz and 30 Hz.
 filters = {f1,f2};
